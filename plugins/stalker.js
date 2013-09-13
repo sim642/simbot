1de810353a3bf959e34bfaaa0dc8f9e6f20a19b5
Simmo Saan <simmo.saan@gmail.com>
2013-09-13 15:56:01 +0300

    Stalker2 now replaces stalker

--- plugins/stalker.js
+++ plugins/stalker.js
@@ -4,74 +4,136 @@ function StalkerPlugin(bot) {
 	self.help = "Stalker plugin";
 	self.depend = ["cmd", "auth"];
 	
-	self.nick2host = {};
-	self.host2nick = {};
+	self.db = {};
+	self.autoid = 1;
+	/*
+		{
+			id:
+			nick:
+			user:
+			host:
+			pid:
+			added:
+			seen:
+		}
+	*/
+	self.ignores = [];
 
 	self.load = function(data) {
 		if (data) {
-			self.nick2host = data.nick2host;
-			self.host2nick = data.host2nick;
+			self.db = data.db;
+			self.autoid = data.autoid;
+			self.ignores = data.ignores;
+			for (var id in self.db) {
+				var row = self.db[id];
+				row.added = new Date(row.added);
+				row.seen = new Date(row.seen);
+			}
 		}
 	};
 
 	self.unload = function() {
 		return {
-			nick2host: self.nick2host,
-			host2nick: self.host2nick
+			"db": self.db,
+			"autoid": self.autoid,
+			"ignores": self.ignores
 		};
 	};
 
-	self.seen = function(nick, host) {
-		if (nick == null || host == null)
-			return;
+	self.seen = function(nick, user, host) {
+		var found = null;
+		for (var id in self.db) {
+			var row = self.db[id];
+			if (row.nick.toLowerCase() == nick.toLowerCase() && row.user.toLowerCase() == user.toLowerCase() && row.host.toLowerCase() == host.toLowerCase()) {
+				found = id;
+				break;
+			}
+		}
 
-		if (!(nick in self.nick2host))
-			self.nick2host[nick] = [];
+		if (found !== null) {
+			self.db[found].seen = new Date();
+		}
+		else {
+			var newrow = {
+				"id": self.autoid++,
+				"nick": nick,
+				"user": user,
+				"host": host,
+				"added": new Date(),
+				"seen": new Date(),
+				"pid": null
+			};
+			
+			if (!self.ignores.some(function (elem, i, arr) {
+				return bot.plugins.auth.match(newrow.nick + "!" + newrow.user + "@" + newrow.host, elem);
+			})) {
+				for (var id in self.db) {
+					var row = self.db[id];
+					if (row.nick.toLowerCase() == nick.toLowerCase() || row.host.toLowerCase() == host.toLowerCase()) {
+						newrow.pid = row.pid !== null ? row.pid : id;
+						break;
+					}
+				}
+			}
 
-		if (!(host in self.host2nick))
-			self.host2nick[host] = [];
-
-		if (self.nick2host[nick].indexOf(host) == -1)
-			self.nick2host[nick].push(host);
-
-		if (self.host2nick[host].indexOf(nick) == -1)
-			self.host2nick[host].push(nick);
+			self.db[newrow.id] = newrow;
+		}
 	};
 
-	self.search = function(nick, host, xnicks, xhosts, depth) {
-		if (depth > 10)
-			return [];
-		else {
-			var nicks = [];
-			if (host !== undefined && !(host in xhosts) && (host in self.host2nick)) {
-				xhosts[host] = null;
-				var ns = self.host2nick[host];
-				nicks = nicks.concat(ns);
+	self.reindex = function() {
+		var newdb = {};
+		for (var id in self.db) {
+			var newrow = self.db[id];
 
-				for (var i = 0; i < ns.length; i++)
-					nicks = nicks.concat(self.search(ns[i], host, xnicks, xhosts, depth + 1));
+			if (!self.ignores.some(function (elem, i, arr) {
+				return bot.plugins.auth.match(newrow.nick + "!" + newrow.user + "@" + newrow.host, elem);
+			})) {
+				for (var id in self.db) {
+					var row = self.db[id];
+					if (row.nick.toLowerCase() == newrow.nick.toLowerCase() || row.host.toLowerCase() == newrow.host.toLowerCase()) {
+						newrow.pid = row.pid !== null ? row.pid : id;
+						break;
+					}
+				}
 			}
 
-			if (nick !== undefined && !(nick in xnicks) && (nick in self.nick2host)) {
-				xnicks[nick] = null;
-				var hs = self.nick2host[nick];
-
-				for (var i = 0; i < hs.length; i++)
-					nicks = nicks.concat(self.search(nick, hs[i], xnicks, xhosts, depth + 1));
-			}
-
-			return nicks;
+			newdb[newrow.id] = newrow;
 		}
+		self.db = newdb;
+	};
+
+	self.stalk = function(nick) {
+		var ids = [];
+		var sid = null, spid = null;
+		for (var id in self.db) {
+			var row = self.db[id];
+			if (row.nick.toLowerCase() == nick.toLowerCase()) {
+				sid = id;
+				spid = row.pid;
+				break;
+			}
+		}
+
+		if (sid !== null) {
+			for (var id in self.db) {
+				var row = self.db[id];
+				if (id == sid || id == spid || (spid !== null && (row.pid == spid || row.pid == spid))) {
+					if (ids.indexOf(id) == -1)
+						ids.push(id);
+				}
+			}
+		}
+		return ids;
 	};
 
 	self.events = {
 		"raw": function(message) {
 			if (message.nick !== undefined && message.host !== undefined) {
-				self.seen(message.nick.toLowerCase(), message.host);
+				self.seen(message.nick, message.user, message.host);
 			}
 			
 			if (message.rawCommand == "352") {
-				self.seen(message.args[5].toLowerCase(), message.args[3]);
+				self.seen(message.args[5], message.args[2], message.args[3]);
 			}
 		},
 
@@ -82,61 +144,47 @@ function StalkerPlugin(bot) {
 		},
 
 		"nick": function(oldNick, newNick, channels, message) {
-			self.seen(newNick.toLowerCase(), message.host);
+			self.seen(newNick, message.user, message.host);
 		},
 
 		"cmd#stalk": function(nick, to, args) {
-			if (args[1]) {
-				var nicks = self.search(args[1].toLowerCase(), undefined, {}, {}, 0);
-				nicks.sort();
-				nicks = nicks.reduce(function(p, c) {
-					if (p.indexOf(c) < 0) p.push(c);
-					return p;
-				}, []);
+			var nick2 = args[1];
+			if (nick2) {
+				var ids = self.stalk(nick2);
+				var nicks = [];
+
+				for (var i = 0; i < ids.length; i++) {
+					var row = self.db[ids[i]];
+					if (nicks.indexOf(row.nick) == -1)
+						nicks.push(row.nick);
+				}
+
+				if (nicks.length == 0)
+					nicks.push("no such nick found");
 				bot.say(to, nick + ": " + nicks.join(", "));
 			}
-		}
-	};
+		},
 
-	self.tolow = function() {
-		var newnick2host = {}, newhost2nick = {};
-		for (var nick in self.nick2host) {
-			newnick2host[nick.toLowerCase()] = self.nick2host[nick];
-		}
+		"cmd#seen": function(nick, to, args) {
+			var nick2 = args[1];
+			if (nick2) {
+				var ids = self.stalk(nick2);
+				var recent = null;
 
-		for (var host in self.host2nick) {
-			var nicks = self.host2nick[host];
-			for (var i = 0; i < nicks.length; i++)
-				nicks[i] = nicks[i].toLowerCase();
-			newhost2nick[host] = nicks;
-		}
+				for (var i = 0; i < ids.length; i++) {
+					var row = self.db[ids[i]];
+					if (recent === null || row.seen > recent.seen)
+						recent = row;
+				}
 
-		self.nick2host = newnick2host;
-		self.host2nick = newhost2nick;
-	};
-
-	self.stripuser = function(host) {
-		return host.substr(host.indexOf("@") + 1);
-	};
-
-	self.stripusers = function() {
-		var newnick2host = {}, newhost2nick = {};
-		for (var nick in self.nick2host) {
-			var hosts = [];// = self.nick2host[nick];
-			for (var i = 0; i <	self.nick2host[nick].length; i++) {
-				if (self.nick2host[nick][i] != null)
-					hosts.push(self.stripuser(self.nick2host[nick][i]));
+				if (recent !== null)
+					bot.say(to, nick2 + " seen as " + recent.nick + ": " + recent.seen.toUTCString() + " UTC");
+				else
+					bot.say(to, nick2 + " has been never seen");
 			}
-			newnick2host[nick] = hosts;
 		}
-
-		for (var host in self.host2nick) {
-			newhost2nick[self.stripuser(host)] = self.host2nick[host];
-		}
-
-		self.nick2host = newnick2host;
-		self.host2nick = newhost2nick;
 	};
+
 }
 
 module.exports = StalkerPlugin;

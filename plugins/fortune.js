4fa9cebcb273bbf81592f740aa2dd7ea360dea3c
Simmo Saan <simmo.saan@gmail.com>
2013-10-22 13:34:32 +0300

    Fortune plugin

---
+++ plugins/fortune.js
@@ -0,0 +1,20 @@
+var exec = require("child_process").exec;
+
+function FortunePlugin(bot) {
+	var self = this;
+	self.name = "fortune";
+	self.help = "Fortune teller plugin";
+	self.depend = ["cmd"];
+
+	self.events = {
+		"cmd#fortune" : function(nick, to, args, message) {
+			exec("fortune -s", function (error, stdout, stderr) {
+				stdout.split("\n").forEach(function (line) {
+					bot.say(to, line);
+				});
+			});
+		}
+	}
+}
+
+module.exports = FortunePlugin;

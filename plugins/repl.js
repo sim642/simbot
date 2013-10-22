cf560b80bb394f7c00aa0ddbcd2a39cd129856eb
Simmo Saan <simmo.saan@gmail.com>
2013-10-22 23:58:59 +0300

    Finish REPL functionality

--- plugins/repl.js
+++ plugins/repl.js
@@ -1,16 +1,45 @@
 var vm = require("vm");
+var util = require("util");
 
 function REPLPlugin(bot) {
 	var self = this;
 	self.name = "repl";
 	self.help = "REPL plugin";
 	self.depend = ["cmd", "auth"];
-
-	self.context = vm.createContext({"bot": bot});
+	
+	self.globalContext = vm.createContext(global);
+	self.contexts = {};
 
 	self.events = {
-		"cmd#>": bot.plugins.auth.proxy(10, function(nick, to, args) {
-			vm.runInContext(args[0].trim(), self.context);
+		"cmd#>": function(nick, to, args) {
+			if (!(to in self.contexts)) {
+				self.contexts[to] = vm.createContext();
+				bot.notice(to, "VM context in " + to + " created");
+			}
+			try {
+				var ret = vm.runInContext(args[0].trim(), self.contexts[to]);
+				self.contexts[to]._ = ret;
+				bot.say(to, nick + " => " + util.inspect(ret, {depth: 0}));
+			} catch (err) {
+				bot.say(to, nick + " => " + err);
+			}
+		},
+
+		"cmd#clear>": function(nick, to, args) {
+			if (to in self.contexts) {
+				delete self.contexts[to];
+				bot.notice(to, "VM context in " + to + " cleared");
+			}
+		},
+		
+		"cmd#>>": bot.plugins.auth.proxy(10, function(nick, to, args) {
+			try {
+				var ret = vm.runInContext(args[0].trim(), self.globalContext);
+				self.globalContext._ = ret;
+				bot.say(to, nick + " =>> " + util.inspect(ret, {depth: 0}));
+			} catch (err) {
+				bot.say(to, nick + " =>> " + err);
+			}
 		})
 	}
 }

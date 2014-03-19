c6317bcd4c04c14b392a8d052d2358b008cf6ce5
Simmo Saan <simmo.saan@gmail.com>
2014-03-19 11:52:45 +0200

    Offensive fortunes support

--- plugins/fortune.js
+++ plugins/fortune.js
@@ -23,6 +23,22 @@ function FortunePlugin(bot) {
 			});
 		},
 
+		"cmd#fortuneo" : function(nick, to, args, message) {
+			var cmd = "fortune -s /usr/share/games/fortunes/off/";
+			if (args[1] && args[1].match(/^\w+$/))
+				cmd += args[1];
+			exec(cmd, function (error, stdout, stderr) {
+				stdout.split("\n").forEach(function (line) {
+					if (line != "")
+						bot.say(to, "  " + line);
+				});
+				stderr.split("\n").forEach(function (line) {
+					if (line != "")
+						bot.say(to, "  " + line);
+				});
+			});
+		},
+
 		"cmd#fortunes": function (nick, to, args) {
 			exec("fortune -f", function (error, stdout, stderr) {
 				var arr = stderr.split("\n").map(function (line) {
@@ -31,6 +47,17 @@ function FortunePlugin(bot) {
 				console.log(arr);
 
 				bot.notice(nick, "All available =fortune categories: " + arr.join(", "));
+			});
+		},
+
+		"cmd#fortuneso": function (nick, to, args) {
+			exec("fortune -f -o", function (error, stdout, stderr) {
+				var arr = stderr.split("\n").map(function (line) {
+					return line.trim().split(" ")[1];
+				}).slice(1, -1);
+				console.log(arr);
+
+				bot.notice(nick, "All available =fortuneo categories: " + arr.join(", "));
 			});
 		}
 	}

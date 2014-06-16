abfa356dbed454582eb24aea93c94cb7ea04664d
Simmo Saan <simmo.saan@gmail.com>
2014-06-16 20:14:24 +0300

    GeoIP allow lookup by domain

--- plugins/geoip.js
+++ plugins/geoip.js
@@ -1,4 +1,5 @@
 var request = require("request");
+var dns = require("dns");
 
 function GeoIPPlugin(bot) {
 	var self = this;
@@ -13,45 +14,75 @@ function GeoIPPlugin(bot) {
 			return "\x02" + key + "\x02";
 	};
 
+	self.geoip = function(ip, callback) {
+		request("http://ipinfo.io/" + ip + "/json", function(err, res, body) {
+			if (!err && res.statusCode == 200) {
+				var j = JSON.parse(body);
+				var bits = [];
+
+				var locstr = "";
+				if (j.city)
+					locstr += j.city + ", ";
+				if (j.region)
+					locstr += j.region + ", ";
+				if (j.country)
+					locstr += j.country;
+				if (locstr != "")
+					bits.push(["location", locstr]);
+
+				if (j.loc && j.loc != "")
+					bits.push(["coords", j.loc]);
+
+				if (j.org)
+					bits.push(["organization", j.org]);
+				if (j.postal)
+					bits.push(["postal code", j.postal]);
+				if (j.bogon)
+					bits.push(["bogon"]);
+
+				var hoststr = j.hostname != "No Hostname" ? " \x02(" + j.hostname + ")\x02" : "";
+				var str = "\x02" + j.ip + hoststr + ": \x02";
+				for (var i = 0; i < bits.length; i++) {
+					str += self.formatPair(bits[i][0], bits[i][1]);
+					if (i != bits.length - 1)
+						str += ", ";
+				}
+
+				(callback || function(){})(str);
+			}
+			else if (!err && res.statusCode == 404) {
+				(callback || function(){})(null);
+			}
+		});
+	};
+
 	self.events = {
 		"cmd#geoip": function(nick, to, args) {
-			request("http://ipinfo.io/" + args[1] + "/json", function(err, res, body) {
-				if (!err && res.statusCode == 200) {
-					var j = JSON.parse(body);
-					var bits = [];
+			self.geoip(args[1], function(str) {
+				if (str === null) {
+					dns.resolve4(args[1], function(err, ips) {
+						if (!err) {
+							var i = args[2] || 1;
+							if (i - 1 < 0 || i - 1 >= ips.length) {
+								bot.say(to, "\x02" + args[1] + "\x02 [" + i + "/" + ips.length + "] invalid resolve index");
+								return;
+							}
 
-					var locstr = "";
-					if (j.city)
-						locstr += j.city + ", ";
-					if (j.region)
-						locstr += j.region + ", ";
-					if (j.country)
-						locstr += j.country;
-					if (locstr != "")
-						bits.push(["location", locstr]);
-
-					if (j.loc && j.loc != "")
-						bits.push(["coords", j.loc]);
-
-					if (j.org)
-						bits.push(["organization", j.org]);
-					if (j.postal)
-						bits.push(["postal code", j.postal]);
-					if (j.bogon)
-						bits.push(["bogon"]);
-
-					var hoststr = j.hostname != "No Hostname" ? " \x02(" + j.hostname + ")\x02" : "";
-					var str = "\x02" + j.ip + hoststr + ": \x02";
-					for (var i = 0; i < bits.length; i++) {
-						str += self.formatPair(bits[i][0], bits[i][1]);
-						if (i != bits.length - 1)
-							str += ", ";
-					}
-
-					bot.say(to, str);
+							var ip = ips[i - 1];
+							self.geoip(ip, function(str) {
+								if (str === null) {
+									bot.say(to, "\x02" + args[1] + "\x02 [" + i + "/" + ips.length + "] = \x02" + ip + "\x02 is invalid IP");
+								}
+								else {
+									bot.say(to, "\x02" + args[1] + "\x02 [" + i + "/" + ips.length + "] = " + str);
+								}
+							});
+						}
+						else
+							bot.say(to, "\x02" + args[1] + "\x02 is invalid IP and domain");
+					});
 				}
-				else if (!err && res.statusCode == 404) {
-					var str = "\x02" + args[1] + ": \x02" + "invalid IP";
+				else {
 					bot.say(to, str);
 				}
 			});

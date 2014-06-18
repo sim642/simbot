e1ef2b13a6d020fdc5b9b044941284ff94940026
Simmo Saan <simmo.saan@gmail.com>
2014-06-18 14:28:53 +0300

    Nickserv ability to check identifiedness

--- plugins/nickserv.js
+++ plugins/nickserv.js
@@ -10,10 +10,28 @@ function NickServPlugin(bot) {
 		self.password = data.password;
 	};
 
+	self.callbacks = {};
+	self.identified = function(nick, callback) {
+		if (!(nick in self.callbacks)) {
+			bot.say("NickServ", "ACC " + nick);
+			self.callbacks[nick] = callback;
+		}
+	};
+
 
 	self.events = {
 		"registered": function() {
 			bot.say("NickServ", "IDENTIFY " + self.password);
+		},
+
+		"notice": function(nick, to, text) {
+			if (nick == "NickServ") {
+				var parts = text.split(" ");
+				if (parts[0] in self.callbacks) {
+					self.callbacks[parts[0]](parts[2] == "3");
+					delete self.callbacks[parts[0]];
+				}
+			}
 		}
 	}
 }

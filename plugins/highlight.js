074ab30f85a6daed60f84b575bba1a2b61103cb8
Simmo Saan <simmo.saan@gmail.com>
2014-06-18 15:02:27 +0300

    Highlights plugin to notice people of being highlighted over pushbullet

---
+++ plugins/highlight.js
@@ -0,0 +1,63 @@
+function HighlightPlugin(bot) {
+	var self = this;
+	self.name = "highlight";
+	self.help = "Highlight plugin";
+	self.depend = ["cmd", "pushbullet"];
+
+	self.highlights = {};
+	/*
+	nick : {
+		online: true/false
+	}
+	*/
+
+	self.load = function(data) {
+		if (data)
+			self.highlights = data.highlights;
+	};
+
+	self.save = function() {
+		return {
+			"highlights": self.highlights
+		};
+	}
+
+	self.events = {
+		"message": function(nick, to, text) {
+			for (var hinick in self.highlights) {
+				if (text.indexOf(hinick) != -1 && !(!self.highlights[hinick].online && (hinick in bot.chans[to].users))) {
+					bot.plugins.pushbullet.pushnote(hinick, "Highlighted in " + to, "<" + nick + "> " + text);
+				}
+			}
+		},
+
+		"cmd#sethighlight": function(nick, to, args) {
+			if (nick in bot.plugins.pushbullet.emails) {
+				switch (args[1]) {
+				case "online":
+					if (!(nick in self.highlights))
+						self.highlights[nick] = {};
+					self.highlights[nick].online = true;
+					bot.say(to, nick + ": highlights set to online");
+					break;
+				case "offline":
+					if (!(nick in self.highlights))
+						self.highlights[nick] = {};
+					self.highlights[nick].online = false;
+					bot.say(to, nick + ": highlights set to offline");
+					break;
+				case "off":
+					if (nick in self.highlights) {
+						delete self.highlights[nick];
+						bot.say(to, nick + ": highlights turned off");
+					}
+					break;
+				}
+			}
+			else
+				bot.say(to, nick + ": pushbullet must be set to use this feature");
+		}
+	}
+}
+
+module.exports = HighlightPlugin;

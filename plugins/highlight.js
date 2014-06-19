4f8b7a9f513361764a597d7fa7a9bc05d8cf9f38
Simmo Saan <simmo.saan@gmail.com>
2014-06-19 09:59:42 +0300

    New away highlight option

--- plugins/highlight.js
+++ plugins/highlight.js
@@ -7,13 +7,20 @@ function HighlightPlugin(bot) {
 	self.highlights = {};
 	/*
 	nick : {
-		online: true/false
+		level: online/away/offline
 	}
 	*/
 
 	self.load = function(data) {
 		if (data)
 			self.highlights = data.highlights;
+
+		for (var nick in self.highlights) {
+			if (self.highlights[nick].online !== undefined) {
+				self.highlights[nick].level = self.highlights[nick].online ? "online" : "offline";
+				delete self.highlights[nick].online;
+			}
+		}
 	};
 
 	self.save = function() {
@@ -25,8 +32,30 @@ function HighlightPlugin(bot) {
 	self.events = {
 		"message": function(nick, to, text) {
 			for (var hinick in self.highlights) {
-				if (text.indexOf(hinick) != -1 && !(!self.highlights[hinick].online && (hinick in bot.chans[to].users))) {
-					bot.plugins.pushbullet.pushnote(hinick, "Highlighted in " + to, "<" + nick + "> " + text);
+				var level = self.highlights[hinick].level;
+				if (text.match(new RegExp("\\b" + hinick)) && !(!(level == "online" || level == "away") && (hinick in bot.chans[to].users))) {
+					switch (level) {
+					case "online":
+						bot.plugins.pushbullet.pushnote(hinick, "Highlighted in " + to, "<" + nick + "> " + text);
+						break;
+					case "offline":
+						if (!(hinick in bot.chans[to].users)) {
+							bot.plugins.pushbullet.pushnote(hinick, "Highlighted in " + to, "<" + nick + "> " + text);
+						}
+						break;
+					case "away":
+						if (!(hinick in bot.chans[to].users)) {
+							bot.plugins.pushbullet.pushnote(hinick, "Highlighted in " + to, "<" + nick + "> " + text);
+						}
+						else {
+							bot.whois(hinick, function(info) {
+								if (info.away !== undefined) {
+									bot.plugins.pushbullet.pushnote(hinick, "Highlighted in " + to, "<" + nick + "> " + text);
+								}
+							});
+						}
+						break;
+					}
 				}
 			}
 		},
@@ -35,16 +64,12 @@ function HighlightPlugin(bot) {
 			if (nick in bot.plugins.pushbullet.emails) {
 				switch (args[1]) {
 				case "online":
-					if (!(nick in self.highlights))
-						self.highlights[nick] = {};
-					self.highlights[nick].online = true;
-					bot.say(nick, "highlights set to online");
-					break;
+				case "away":
 				case "offline":
 					if (!(nick in self.highlights))
 						self.highlights[nick] = {};
-					self.highlights[nick].online = false;
-					bot.say(nick, "highlights set to offline");
+					self.highlights[nick].level = args[1];
+					bot.say(nick, "highlights set to " + args[1]);
 					break;
 				case "off":
 					if (nick in self.highlights) {

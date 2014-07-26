4d9c8dae70e331afc75e67f498722a41df2164c9
Simmo Saan <simmo.saan@gmail.com>
2014-07-26 10:12:06 +0300

    Allow greeting both ways

--- plugins/greet.js
+++ plugins/greet.js
@@ -3,13 +3,23 @@ function GreetPlugin(bot) {
 	self.name = "greet";
 	self.help = "Greet plugin";
 	
-	self.re = /(yo(?:[\s-]yo)*|h[ea]l{2,}o+|h[ae]+[jiy]+l?|h[ei]+(?:-?y[ao]+)?|howdy|hola|bonjour|(?:what(?:\')?s)?\sup|fuck(?:ing|\soff|\syou(?:\stoo|2)?)?)\s([^!\s]+)/i
+	self.midre = /(yo(?:[\s-]yo)*|h[ea]l{2,}o+|h[ae]+[jiy]+l?|h[ei]+(?:-?y[ao]+)?|howdy|hola|bonjour|(?:what(?:\')?s)?\sup|fuck(?:ing|\soff|\syou(?:\stoo|2)?)?)/;
 
 	self.events = {
 		"message": function(nick, to, text) {
-			var m = text.match(self.re);
-			if (m && m[2].toLowerCase() == bot.nick.toLowerCase()) {
-				bot.say(to, m[1] + " " + nick);
+			var re = new RegExp("(\\b" + bot.nick + "[\\s,:]+)?" + self.midre.source + "(\\s+" + bot.nick + "\\b)?", "i");
+			var m = text.match(re);
+			
+			
+			/*if (m && (m[1] !== undefined || m[3] !== undefined)) {
+				bot.say(to, m[2] + " " + nick);
+			}*/
+
+			if (m) {
+				if (m[1] !== undefined)
+					bot.say(to, m[1].replace(bot.nick, nick) + m[2]);
+				else if (m[3] !== undefined)
+					bot.say(to, m[2] + m[3].replace(bot.nick, nick));
 			}
 		}
 	}

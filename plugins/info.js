17ea31a135ba1d395a9665cb9e8ab6d0e81085be
Simmo Saan <simmo.saan@gmail.com>
2014-08-03 10:39:06 +0300

    Bring back multiinfo interface to be reimplemented for Awfulnet/Infobot
    This reverts commit fd541e814786dfef799b33c05724307cb3d33c5e.

--- plugins/info.js
+++ plugins/info.js
@@ -6,25 +6,39 @@ function InfoPlugin(bot) {
 	
 	self.callbacks = [];
 
-	self.info = function(nick, callback) {
-		bot.say("infobot", "!info " + nick);
-		self.callbacks.push({"nick": nick, "callback": callback});
+	self.info = function(nicks, callback) {
+		bot.say("infobot", "!mquery " + nicks.join(","));
+		for (var i = 0; i < nicks.length; i++) {
+			var item = {"nick": nicks[i], "callback": callback};
+			if (self.callbacks[nicks[i]] === undefined)
+				self.callbacks[nicks[i]] = [item];
+			else
+				self.callbacks[nicks[i]].push(item);
+		}
 	};
 
 	self.invalidRe = /^Invalid target\. Was (.+) identified with NickServ when it was \!added\?$/;
+	self.nickRe = /^([a-z_\-\[\]\\^{}|`][a-z0-9_\-\[\]\\^{}|`]*): (.*)$/i;
 
 	self.events = {
 		"notice": function(nick, to, text, message) {
-			if (nick == "infobot" && to == bot.nick && self.callbacks.length > 0) {
-				var callback = self.callbacks.shift();
-				if (text.match(self.invalidRe))
-					text = undefined;
-				callback.callback(text, callback.nick);
+			if (nick == "infobot" && to == bot.nick) {
+				var m = text.match(self.nickRe);
+				if (m[1] in self.callbacks) {
+					var callback = self.callbacks[m[1]];
+					for (var i = 0; i < callback.length; i++) {
+						var info = m[2];
+						if (info.match(self.invalidRe))
+							info = undefined;
+						callback[i].callback(info, callback[i].nick);
+					}
+					delete self.callbacks[m[1]];
+				}
 			}
 		},
 
 		"cmd#infobot": function(nick, to, args, message) {
-			self.info(args[1], function(text, nick) {
+			self.info(args[1].split(","), function(text, nick) {
 				bot.say(to, "Info of " + nick + ": " + text);
 			});
 		}

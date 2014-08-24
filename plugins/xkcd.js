a226e5c8bf7163dd7df158f7653f9a618e5a3197
Simmo Saan <simmo.saan@gmail.com>
2014-08-24 19:14:08 +0300

    XKCD plugin search result index, neater formatting

--- plugins/xkcd.js
+++ plugins/xkcd.js
@@ -27,23 +27,41 @@ function XkcdPlugin(bot) {
 					var re = /<div id="ctitle">(.+)<\/div>/;
 					var m = body.match(re);
 					if (m)
-						bot.say(to, "xkcd: " + m[1] + " - " + uri);
+						bot.say(to, "xkcd" + (args[1] ? " #" + args[1] : "") + ": \x02" + m[1] + "\x02 - " + uri);
 				});
 			}
 			else {
+				var match = args[0].match(/^(.*)\s+(\d+)\s*$/);
+				var str;
+				var i;
+				if (match) {
+					str = match[1];
+					i = parseInt(match[2]);
+				}
+				else {
+					str = args[0];
+					i = 1;
+				}
+				str = str.trim();
+
 				request({
 						uri: "https://www.googleapis.com/customsearch/v1",
 						qs: {
 							key: self.apiKey,
 							cx: "012652707207066138651:zudjtuwe28q",
-							q: args[0]
+							q: str
 						}
 					}, function(err, res, body) {
 					if (!err && res.statusCode == 200) {
 						var json = JSON.parse(body);
 						if (json.items !== undefined && json.items.length != 0) {
-							var first = json.items[0];
-							bot.say(to, first.title + " - " + first.link);
+							if (i - 1 >= 0 && i - 1 < json.items.length) {
+								var item = json.items[i - 1];
+								var title = item.title.match(/^xkcd:(.*)$/)[1];
+								bot.say(to, "xkcd '" + str + "' [" + i + "/" + json.items.length + "]: \x02" + title + "\x02 - " + item.link);
+							}
+							else
+								bot.say(to, nick + ": '" + str + "' [" + i + "/" + json.items.length + "] invalid result index");
 						}
 						else
 							bot.say(to, nick + ": couldn't find xkcd");

a1ffa36ebe2e26b0a804c8e6c857da6794129c69
Simmo Saan <simmo.saan@gmail.com>
2014-08-30 14:27:01 +0300

    watch: list and stat calls

--- plugins/watch.js
+++ plugins/watch.js
@@ -4,6 +4,12 @@ function WatchPlugin(bot) {
 	self.help = "WATCH plugin";
 	self.depend = [];
 
+	self.listArray = [];
+	self.listCallbacks = [];
+
+	self.statData = {};
+	self.statCallbacks = [];
+
 	self.watch = function(args) {
 		args.unshift("WATCH");
 		bot.send.apply(bot, args);
@@ -21,6 +27,22 @@ function WatchPlugin(bot) {
 		self.watch(["-" + from, "+" + to]);
 	};
 
+	self.list = function(callback) {
+		if (self.listCallbacks.length == 0)
+			self.watch(["l"]);
+
+		self.listCallbacks.push(callback);
+	};
+
+	self.stat = function(callback) {
+		if (self.statCallbacks.length == 0) {
+			self.watch(["s"]);
+			self.statData.list = [];
+		}
+
+		self.statCallbacks.push(callback);
+	};
+
 	self.emitEvents = {
 		"600": "watch#logon",
 		"601": "watch#logoff",
@@ -31,10 +53,10 @@ function WatchPlugin(bot) {
 	self.events = {
 		"raw": function(message) {
 			switch (message.rawCommand) {
-			case "600":
-			case "601":
-			case "604":
-			case "605":
+			case "600": // logon
+			case "601": // logoff
+			case "604": // nowon
+			case "605": // nowoff
 				var watch = {
 					nick: message.args[1],
 					user: message.args[2],
@@ -42,7 +64,42 @@ function WatchPlugin(bot) {
 					logon: new Date(message.args[4] * 1000)
 				};
 
+				if (message.rawCommand == "604" && self.listCallbacks.length > 0) {
+					self.listArray.push(watch);
+				}
+
 				bot.emit(self.emitEvents[message.rawCommand], watch);
+				break;
+
+			case "603": // watchstat
+				var re = /You have (\d+) and are on (\d+) WATCH entries/;
+				var match = message.args[1].match(re);
+				if (match && self.statCallbacks.length > 0) {
+					self.statData.have = parseInt(match[1]);
+					self.statData.on = parseInt(match[2]);
+				}
+				break;
+
+			case "606": // watchlist
+				if (self.statCallbacks.length > 0) {
+					self.statData.list = self.statData.list.concat(message.args[1].split(" "));
+				}
+				break;
+
+			case "607": // end of watchlist
+				for (var i = 0; i < self.listCallbacks.length; i++) {
+					self.listCallbacks[i](self.listArray);
+				}
+
+				for (var i = 0; i < self.statCallbacks.length; i++) {
+					var data = self.statData;
+					self.statCallbacks[i](data.have, data.on, data.list);
+				}
+
+				self.listCallbacks = [];
+				self.listArray = [];
+				self.statCallbacks = [];
+				self.statData = {};
 				break;
 			}
 		}

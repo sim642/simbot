4818a9c9d831e211adc9dd2173e8b6cdfcc6a384
Simmo Saan <simmo.saan@gmail.com>
2014-09-27 19:02:23 +0300

    greet: don't match ' up'

--- plugins/greet.js
+++ plugins/greet.js
@@ -3,7 +3,7 @@ function GreetPlugin(bot) {
 	self.name = "greet";
 	self.help = "Greet plugin";
 	
-	self.midre = /(yo(?:[\s-]yo)*|h[ea]l{2,}o+|h[ae]+[jiy]+[aol]*|hi+(?:-?y[ao]+)?|howdy|hola|bonjour|(?:what(?:\')?s)?\sup|fuck(?:ing|\soff|\syou(?:\stoo|2)?)?)/;
+	self.midre = /(yo(?:[\s-]yo)*|h[ea]l{2,}o+|h[ae]+[jiy]+[aol]*|hi+(?:-?y[ao]+)?|howdy|hola|bonjour|(?:what(?:\')?s\s|s)up|fuck(?:ing|\soff|\syou(?:\stoo|2)?)?)/;
 
 	self.events = {
 		"message": function(nick, to, text) {

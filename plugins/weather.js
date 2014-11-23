ce57890ea9578cbdeb333c246b7dcd780ae61e30
Simmo Saan <simmo.saan@gmail.com>
2014-11-23 21:02:02 +0200

    weather: long-term forecast

--- plugins/weather.js
+++ plugins/weather.js
@@ -163,6 +163,69 @@ function WeatherPlugin(bot) {
 					}
 				}
 			});
+		},
+
+		"cmd#forecast2": function(nick, to, args) {
+			var place = args[1];
+			var time = new Date(args[2])
+			var t = time.getTime() / 1000;
+
+			request("http://api.openweathermap.org/data/2.5/forecast/daily?lang=en&q=" + args[1], function(err, res, body) {
+				if (!err && res.statusCode == 200) {
+					var jj = JSON.parse(body);
+					if (jj.cod == 200) {
+						for (var i = 0; i < jj.cnt; i++) {
+							var j = jj.list[i];
+							if (t >= j.dt && t < (i + 1 < jj.cnt ? jj.list[i + 1].dt : j.dt + 24 * 60 * 60)) {
+								var bits = [];
+								bits.push(["temperature", (j.temp.day - 273.15).toFixed(1) + "°C"]); // TODO: time of day temperature
+								bits.push(["humidity", j.humidity.toString() + "%"]);
+								bits.push(["pressure", j.pressure.toString() + "hPa"]);
+								bits.push(["wind", j.speed.toString() + "m/s " + j.deg.toFixed(0).toString() + "°"]);
+								bits.push(["clouds", j.clouds.toString() + "%"]);
+								if (j.rain !== undefined) {
+									bits.push(["rain", j.rain.toString() + "mm/3h"]); // correct unit?
+								}
+								if (j.snow) {
+									bits.push(["snow", j.snow.toString() + "mm/3h"]); // correct unit?
+								}
+								if (j.weather) {
+									var val = "";
+									for (var i = 0; i < j.weather.length; i++) {
+										val += j.weather[i].description;
+										if (i != j.weather.length - 1)
+											val += ", ";
+									}
+									bits.push(["conditions", val]);
+								}
+								/*if (j.sys.sunrise) {
+									var d = self.DateUTC(new Date(j.sys.sunrise * 1000));
+									bits.push(["sunrise", d.toTimeString().split(" ")[0]]);
+								}
+								if (j.sys.sunset) {
+									var d = self.DateUTC(new Date(j.sys.sunset * 1000));
+									bits.push(["sunset", d.toTimeString().split(" ")[0]]);
+								}*/
+
+								var str = "\x02" + jj.city.name + ", " + jj.city.country + " @ " + time.toUTCString() + ": \x02";
+								for (var i = 0; i < bits.length; i++) {
+									str += self.formatPair(bits[i][0], bits[i][1]);
+									if (i != bits.length - 1)
+										str += ", ";
+								}
+
+								bot.say(to, str);
+								return;
+							}
+						}
+
+						bot.say(to, "No forecast found for \x02" + jj.city.name + ", " + jj.city.country + " @ " + time.toUTCString());
+					}
+					else {
+						bot.say(to, "No place called \x02" + place);
+					}
+				}
+			});
 		}
 	}
 }

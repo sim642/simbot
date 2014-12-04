77b8b933e6cd39a1f5e1d231dd4e3844852e28df
Simmo Saan <simmo.saan@gmail.com>
2014-12-04 16:52:22 +0200

    whatpulse: new plugin

---
+++ plugins/whatpulse.js
@@ -0,0 +1,59 @@
+var request = require("request");
+
+function WhatpulsePlugin(bot) {
+	var self = this;
+	self.name = "whatpulse";
+	self.help = "Whatpulse plugin";
+	self.depend = ["cmd", "nickserv"];
+
+	self.users = {};
+
+	self.load = function(data) {
+		if (data && data.users)
+			self.users = data.users;
+	};
+
+	self.save = function() {
+		return {
+			"users": self.users
+		};
+	};
+
+	self.parseuser = function(user) {
+		if (user in self.users)
+			return self.users[user];
+		else
+			return user;
+	};
+
+	self.events = {
+		"cmd#whatpulse": function(nick, to, args) {
+			var user = self.parseuser((args[1] || nick).toLowerCase());
+			request("http://api.whatpulse.org/user.php?format=json&user=" + user, function(err, res, body) {
+				if (!err && res.statusCode == 200) {
+					var data = JSON.parse(body);
+					bot.say(to, data.AccountName + ": " + data.Keys);
+				}
+			});
+		},
+
+		"cmd#setwhatpulse": function(nick, to, args) {
+			bot.plugins.nickserv.nickIdentified(nick, function(identified) {
+				if (identified) {
+					if (args[1] !== undefined) {
+						self.users[nick.toLowerCase()] = args[1];
+						bot.say(nick, "whatpulse set to " + args[1]);
+					}
+					else {
+						delete self.users[nick.toLowerCase()];
+						bot.say(nick, "whatpulse unset");
+					}
+				}
+				else
+					bot.say(nick, "must be identified for this nick to set whatpulse");
+			});
+		}
+	}
+}
+
+module.exports = WhatpulsePlugin;

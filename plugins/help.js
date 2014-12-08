2d6cf6e15fbfab326f410fdd44d5fffd2aa42327
Simmo Saan <simmo.saan@gmail.com>
2014-12-08 13:47:00 +0200

    help: link to github wiki

--- plugins/help.js
+++ plugins/help.js
@@ -1,21 +1,29 @@
+var request = require("request");
+
 function HelpPlugin(bot) {
 	var self = this;
 	self.name = "help";
 	self.help = "Help plugin";
 	self.depend = ["cmd"];
 
+	self.github = "https://github.com/sim642/simbot";
+
 	self.events = {
 		"cmd#help": function(nick, to, args, message) {
-			if (bot.plugins[args[1]])
-				bot.say(to, nick + ": " + args[1] + " - " + bot.plugins[args[1]].help);
+			if (bot.plugins[args[1]]) {
+				var url = self.github + "/wiki/" + args[1];
+				request.head({"url": url, followRedirect: false}, function(err, res, body) {
+					bot.say(to, nick + ": " + args[1] + " - " + bot.plugins[args[1]].help + (!err && res.statusCode == 200 ? " - " + url : ""));
+				});
+			}
 			else if (args[1])
 				bot.say(to, nick + ": no such plugin '" + args[1] + "'");
 			else
-				bot.say(to, nick + ": https://github.com/sim642/simbot/wiki");
+				bot.say(to, nick + ": " + self.github + "/wiki");
 		},
 
 		"ctcp-version": function(from, to, message) {
-			bot.ctcp(from, "notice", "VERSION simbot by sim642: https://github.com/sim642/simbot");
+			bot.ctcp(from, "notice", "VERSION simbot by sim642: " + self.github);
 		}
 	}
 }

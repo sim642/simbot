b1f67a97ca22bf23fb3b2645eb0203451c7c0285
Simmo Saan <simmo.saan@gmail.com>
2014-12-13 15:45:32 +0200

    pushbullet: real-time push receiving, subscriptions

--- plugins/pushbullet.js
+++ plugins/pushbullet.js
@@ -1,4 +1,5 @@
 var request = require("request");
+var WebSocket = require("ws");
 
 function PushbulletPlugin(bot) {
 	var self = this;
@@ -8,9 +9,32 @@ function PushbulletPlugin(bot) {
 
 	self.token = null;
 	self.emails = {};
+	self.ws = null;
+	self.lastTs = Date.now() / 1000;
+
+	self.setToken = function(token) {
+		self.token = token;
+		self.ws = new WebSocket("wss://stream.pushbullet.com/websocket/" + self.token);
+		self.ws.on("message", function(message) {
+			var data = JSON.parse(message);
+			if (data.type == "tickle" && data.subtype == "push") {
+				self.getPushes(self.lastTs, function(pushes) {
+					if (pushes[0])
+						self.lastTs = pushes[0].modified;
+
+					pushes.forEach(function(push) {
+						if (push.receiver_email == "sim642bot@gmail.com")
+							bot.emit("pushbullet#push", push);
+						else if (push.channel_iden)
+							bot.emit("pushbullet#subscription", push);
+					});
+				});
+			}
+		});
+	}
 
 	self.load = function(data) {
-		self.token = data.token;
+		self.setToken(data.token);
 		if (data.emails)
 			self.emails = data.emails;
 	};
@@ -22,11 +46,37 @@ function PushbulletPlugin(bot) {
 		};
 	};
 
+	self.disable = function() {
+		self.ws.close();
+	};
+
 	self.parseto = function(to) {
 		if (to in self.emails)
 			return self.emails[to];
 		else
 			return to;
+	};
+
+	self.getPushes = function(lastTs, callback) {
+		request({
+			url: "https://api.pushbullet.com/v2/pushes",
+			auth: {
+				user: self.token
+			},
+			json: true,
+			qs: {
+				"modified_after": lastTs
+			}
+		}, function (err, res, body) {
+			if (!err && res.statusCode == 200) {
+				(callback || function(){})(body.pushes);
+			}
+			else {
+				bot.out.error("pushbullet", err);
+				bot.out.error("pushbullet", body);
+				(callback || function(){})(undefined);
+			}
+		});
 	};
 
 	self.push = function(params, callback) {
@@ -59,7 +109,40 @@ function PushbulletPlugin(bot) {
 		}, callback);
 	};
 
+	self.subscribe = function(channel, callback) {
+		request.post({
+			url: "https://api.pushbullet.com/v2/subscriptions",
+			auth: {
+				user: self.token
+			},
+			json: true,
+			body: {
+				"channel_tag": channel
+			}
+		}, function(err, res, body) {
+			if (!err && res.statusCode == 200) {
+				(callback || function(){})(body);
+			}
+			else {
+				bot.out.error("pushbullet", err);
+				bot.out.error("pushbullet", body);
+				(callback || function(){})(body);
+			}
+
+		});
+	};
+
 	self.events = {
+		"pushbullet#push": function(push) {
+			bot.out.log("pushbullet", "Push from " + push.sender_email + ": " + push.title);
+			bot.emit("pushbullet#push#" + push.sender_email, push);
+		},
+
+		"pushbullet#subscription": function(push) {
+			bot.out.log("pushbullet", "Subscription from " + push.sender_name + ": " + push.title);
+			bot.emit("pushbullet#subscription#" + push.sender_name, push);
+		},
+
 		"cmd#pushbullet": bot.plugins.auth.proxy(6, function(nick, to, args) {
 			self.pushnote(args[1], args[2], args[3], function(err) {
 				if (err)

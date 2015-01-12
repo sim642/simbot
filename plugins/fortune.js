4ec06e1e26a9c7ef44db23f1eb925c1533ae0dc5
Simmo Saan <simmo.saan@gmail.com>
2015-01-12 22:57:09 +0200

    fortune: notify command caller about error

--- plugins/fortune.js
+++ plugins/fortune.js
@@ -12,6 +12,9 @@ function FortunePlugin(bot) {
 			if (args[1] && args[1].match(/^\w+$/))
 				arr.push(args[1]);
 			execFile("fortune", arr, {timeout: 100}, function (error, stdout, stderr) {
+				if (error && error.killed == true)
+					bot.say(to, nick + ": no such fortune found");
+
 				stdout.split("\n").forEach(function (line) {
 					if (line != "")
 						bot.say(to, "  " + line);
@@ -28,6 +31,9 @@ function FortunePlugin(bot) {
 			if (args[1] && args[1].match(/^\w+$/))
 				arr.push(args[1]);
 			execFile("fortune", arr, {timeout: 100}, function (error, stdout, stderr) {
+				if (error && error.killed == true)
+					bot.say(to, nick + ": no such fortune found");
+
 				stdout.split("\n").forEach(function (line) {
 					if (line != "")
 						bot.say(to, "  " + line);

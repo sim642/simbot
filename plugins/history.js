57e75f5b5a68c9ee2a87df945d2e5d00932ef576
Simmo Saan <simmo.saan@gmail.com>
2015-02-13 17:05:21 +0200

    history: generic log iteration function, make =history use that

--- plugins/history.js
+++ plugins/history.js
@@ -20,46 +20,61 @@ function HistoryPlugin(bot) {
 		return {basedir: self.basedir, basename: self.basename};
 	}
 
+	self.iterate = function(channel, callback) {
+		fs.readdir(self.basedir, function(err, files) {
+			var re = new RegExp("^" + self.basename + channel + "_\\d{8}\\.log$");
+			var logfiles = files.filter(function (file) {
+				return file.match(re);
+			}).sort();
+
+			outer:
+			for (var i = logfiles.length - 1; i >= 0; i--) {
+				var lines = fs.readFileSync(self.basedir + logfiles[i]).toString().split("\n");
+
+				for (var j = lines.length - 1 - 1; j >= 0; j--) {
+					if (!callback(lines[j]))
+						break outer;
+				}
+			}
+		});
+	};
+
 	self.events = {
 		"cmd#history": function(nick, to, args) {
-			fs.readdir(self.basedir, function(err, files) {
-				var linecnt;
-				var channel = to;
-				if (args[1] !== undefined && args[1].match(/^#/)) {
-					linecnt = args[2];
-					channel = args[1];
+			var linecnt;
+			var channel = to;
+			if (args[1] !== undefined && args[1].match(/^#/)) {
+				linecnt = args[2];
+				channel = args[1];
+			}
+			else
+				linecnt = args[1];
+
+			linecnt = Math.min(linecnt || 15, 50) + 1;
+
+			if (!channel.match(/^#/))
+				return;
+
+			var outlines = [];
+
+			self.iterate(channel, function(line) {
+				outlines.unshift(line);
+				linecnt--;
+
+				if (linecnt == 0) {
+					if (channel == to)
+						outlines.pop();
+					
+					bot.say(nick, "--- Begin history for " + channel + " ---");
+					for (var i = 0; i < outlines.length; i++) {
+						bot.say(nick, outlines[i]);
+					}
+					bot.say(nick, "--- End history for " + channel + " ---");
+
+					return false;
 				}
 				else
-					linecnt = args[1];
-
-				linecnt = Math.min(linecnt || 15, 50) + 1;
-
-				if (!channel.match(/^#/))
-					return;
-
-				var outlines = [];
-
-				var re = new RegExp("^" + self.basename + channel + "_\\d{8}\\.log$");
-				var logfiles = files.filter(function (file) {
-					return file.match(re);
-				}).sort();
-
-				for (var i = logfiles.length - 1; i >= 0 && linecnt > 0; i--) {
-					var lines = fs.readFileSync(self.basedir + logfiles[i]).toString().split("\n");
-					
-					for (var j = lines.length - 1 - 1; j >= 0 && linecnt > 0; j--) {
-						outlines.unshift(lines[j]);
-						linecnt--;
-					}
-				}
-
-				outlines.pop();
-				
-				bot.say(nick, "--- Begin history for " + channel + " ---");
-				for (var i = 0; i < outlines.length; i++) {
-					bot.say(nick, outlines[i]);
-				}
-				bot.say(nick, "--- End history for " + channel + " ---");
+					return true;
 			});
 		}
 	}

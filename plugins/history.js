c6e5f9327d91077c05f4994de7ecb45cfee20f33
Simmo Saan <simmo.saan@gmail.com>
2015-02-13 18:08:31 +0200

    history: end of iteration callback

--- plugins/history.js
+++ plugins/history.js
@@ -20,22 +20,24 @@ function HistoryPlugin(bot) {
 		return {basedir: self.basedir, basename: self.basename};
 	}
 
-	self.iterate = function(channel, callback) {
+	self.iterate = function(channel, lineCb, endCb) {
 		fs.readdir(self.basedir, function(err, files) {
 			var re = new RegExp("^" + self.basename + channel + "_\\d{8}\\.log$");
 			var logfiles = files.filter(function (file) {
 				return file.match(re);
 			}).sort();
 
-			outer:
-			for (var i = logfiles.length - 1; i >= 0; i--) {
+			var found = false;
+			for (var i = logfiles.length - 1; !found && i >= 0; i--) {
 				var lines = fs.readFileSync(self.basedir + logfiles[i]).toString().split("\n");
 
-				for (var j = lines.length - 1 - 1; j >= 0; j--) {
-					if (!callback(lines[j]))
-						break outer;
+				for (var j = lines.length - 1 - 1; !found && j >= 0; j--) {
+					if (!lineCb(lines[j]))
+						found = true;
 				}
 			}
+
+			(endCb || function(){})(found);
 		});
 	};
 
@@ -61,20 +63,16 @@ function HistoryPlugin(bot) {
 				outlines.unshift(line);
 				linecnt--;
 
-				if (linecnt == 0) {
-					if (channel == to)
-						outlines.pop();
-					
-					bot.say(nick, "--- Begin history for " + channel + " ---");
-					for (var i = 0; i < outlines.length; i++) {
-						bot.say(nick, outlines[i]);
-					}
-					bot.say(nick, "--- End history for " + channel + " ---");
+				return linecnt > 0;
+			}, function() {
+				if (channel == to)
+					outlines.pop();
 
-					return false;
+				bot.say(nick, "--- Begin history for " + channel + " ---");
+				for (var i = 0; i < outlines.length; i++) {
+					bot.say(nick, outlines[i]);
 				}
-				else
-					return true;
+				bot.say(nick, "--- End history for " + channel + " ---");
 			});
 		}
 	}

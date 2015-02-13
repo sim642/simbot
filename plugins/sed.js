b0451e784cce4d6655fb0b446ea58e0cf6871f04
Simmo Saan <simmo.saan@gmail.com>
2015-02-13 18:16:23 +0200

    sed: logging

--- plugins/sed.js
+++ plugins/sed.js
@@ -10,6 +10,8 @@ function SedPlugin(bot) {
 		"message": function(nick, to, text) {
 			var m = text.match(self.sedRe);
 			if (m) {
+				bot.out.log("cmd", nick + " in " + to + ": " + m[0]);
+
 				var sedNick = m[1] || nick;
 				var sedPrere = new RegExp(m[2] || ".*");
 				var sedRe = new RegExp(m[3], m[5]);
@@ -17,9 +19,9 @@ function SedPlugin(bot) {
 
 				var re = new RegExp("^\\[[\\d:]{8}\\] <" + sedNick + "> (.*)$");
 
-				var cnt = 1000; // TEMPORARY TESTING LIMIT
+				var cnt = 0;
 				bot.plugins.history.iterate(to, function(line) {
-					cnt--;
+					cnt++;
 					var m2 = line.match(re);
 					if (m2) {
 						var text2 = m2[1];
@@ -32,7 +34,9 @@ function SedPlugin(bot) {
 						}
 					}
 
-					return cnt >= 0;
+					return true;
+				}, function(found) {
+					bot.out.debug("sed", "line " + cnt + " " + found);
 				});
 			}
 		}

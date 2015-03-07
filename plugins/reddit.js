cff472a95ed8c6c602597b92197a3ab8cc1feb5d
Simmo Saan <simmo.saan@gmail.com>
2015-03-07 09:46:13 +0200

    reddit: new relevant reddit post finder plugin

---
+++ plugins/reddit.js
@@ -0,0 +1,67 @@
+var request = require("request");
+var url = require("url");
+
+function RedditPlugin(bot) {
+	var self = this;
+	self.name = "reddit";
+	self.help = "Reddit plugin";
+	self.depend = ["auth", "cmd"];
+
+	self.urlRe = /(https?|ftp):\/\/[^\s\/$.?#].[^\s]*/i;
+
+	self.channels = [];
+	self.ignores = [];
+
+	self.load = function(data) {
+		self.channels = data.channels;
+		self.ignores = data.ignores;
+	};
+
+	self.save = function() {
+		return {channels: self.channels, ignores: self.ignores};
+	};
+
+	self.lookup = function(lurl, callback) {
+		request("https://www.reddit.com/search.json?q=url:" + lurl, function(err, res, body) {
+			if (!err && res.statusCode == 200) {
+				var data = JSON.parse(body).data;
+				var results = data.children;
+
+				if (results.length > 0) {
+					var post = results[0].data;
+					var str = url.resolve("http://www.reddit.com/", post.permalink);
+					(callback || function(){})(str);
+				}
+			}
+		});
+	};
+
+	self.events = {
+		"message": function(nick, to, text, message) {
+			if ((self.channels.indexOf(to) != -1) &&
+				!self.ignores.some(function (elem, i, arr) {
+					return bot.plugins.auth.match(message.nick + "!" + message.user + "@" + message.host, elem);
+				})) {
+				var match = text.match(self.urlRe);
+				if (match) {
+					bot.out.log("reddit", nick + " in " + to + ": " + match[0]);
+					self.lookup(match[0], function(str) {
+						bot.say(to, str);
+					});
+				}
+			}
+		},
+
+		/*"pm": function(nick, text, message) {
+			var match = text.match(self.vidre);
+			if (match) {
+				bot.out.log("youtube", nick + " in PM: " + match[0]);
+				self.lookup(match[1], function(str) {
+					bot.say(nick, str);
+				});
+			}
+		}*/
+	}
+}
+
+module.exports = RedditPlugin;

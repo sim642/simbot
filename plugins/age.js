af356a5dabb852fe0f16ee862744cface2b51bc3
Simmo Saan <simmo.saan@gmail.com>
2015-04-04 19:46:42 +0300

    age: revive plugin with proper functions

---
+++ plugins/age.js
@@ -0,0 +1,18 @@
+function AgePlugin(bot) {
+	var self = this;
+	self.name = "age";
+	self.help = "Age calculator plugin";
+	self.depend = ["cmd", "date"];
+
+	self.events = {
+		"cmd#age": function(nick, to, args) {
+			var birth = bot.plugins.date.toUTC(new Date(args[0]));
+			var years = (Date.now() - birth.getTime()) / (1000 * 60 * 60 * 24 * 365.24);
+			bot.say(to, nick + " is " + bot.plugins.date.printDur(bot.plugins.date.toUTC(new Date(args[0])), "second") + " (" + years.toFixed(3) + " years) old in UTC");
+		}
+	};
+
+}
+
+module.exports = AgePlugin;
+

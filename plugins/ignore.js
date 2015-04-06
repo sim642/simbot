7da3468d08da252494f1b767946a81b58ac9dee5
Simmo Saan <simmo.saan@gmail.com>
2015-04-06 18:52:39 +0300

    ignore: caller independent ignore list

--- plugins/ignore.js
+++ plugins/ignore.js
@@ -4,8 +4,19 @@ function IgnorePlugin(bot) {
 	self.help = "Ignore back-end plugin";
 	self.depend = ["auth"];
 
+	self.ignores = [];
+
+	self.load = function(data) {
+		if (data && data.ignores)
+			self.ignores = data.ignores;
+	};
+
+	self.save = function() {
+		return {ignores: self.ignores};
+	};
+
 	self.ignored = function(ignores, message) {
-		return ignores.some(function (elem, i, arr) {
+		return self.ignores.concat(ignores).some(function (elem, i, arr) {
 				return bot.plugins.auth.match(message.nick + "!" + message.user + "@" + message.host, elem);
 			});
 	};

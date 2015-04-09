153512bfc4a8a7f72d52c4a1a60ab312a0cfc9d7
Simmo Saan <simmo.saan@gmail.com>
2015-04-09 23:12:23 +0300

    greet: don't say "hail"

--- plugins/greet.js
+++ plugins/greet.js
@@ -3,7 +3,7 @@ function GreetPlugin(bot) {
 	self.name = "greet";
 	self.help = "Greet plugin";
 	
-	self.midre = /\b(yo(?:[\s-]yo)*|h[ea]l{2,}o+|h[ae]+[jiy]+[aol]*|hi+(?:-?y[ao]+)?|howdy|hola|bonjour|(?:what(?:\')?s\s|s)up|fuck(?:ing|\soff|\syou(?:\stoo|2)?)?)\b/;
+	self.midre = /\b(yo(?:[\s-]yo)*|h[ea]l{2,}o+|h[ae]+[jiy]+[ao]*|hi+(?:-?y[ao]+)?|howdy|hola|bonjour|(?:what(?:\')?s\s|s)up|fuck(?:ing|\soff|\syou(?:\stoo|2)?)?)\b/;
 
 	self.events = {
 		"message": function(nick, to, text) {

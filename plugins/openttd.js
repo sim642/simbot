6f8b501f540e55b2dd70498b174ce5139875ff43
Simmo Saan <simmo.saan@gmail.com>
2015-05-12 00:12:37 +0300

    openttd: company lookup command

--- plugins/openttd.js
+++ plugins/openttd.js
@@ -25,7 +25,7 @@ function OpenTTDPlugin(bot) {
 			var server = args[1] || self.defServer;
 			var port = args[2] || self.defPort;
 			openttd.query(server, port, function(ret) {
-				var prefix = ret[1].name + " (" + server + ":" + port + ")";
+				var prefix = ret[1].name + " \x02(" + server + ":" + port + ")\x02";
 
 				var bits = [];
 				bits.push(["map", ret[1].map.name + " \x02(" + ret[1].map.width + "×" + ret[1].map.height + ")\x02"]);
@@ -52,6 +52,45 @@ function OpenTTDPlugin(bot) {
 				companies.forEach(function(company) {
 					bits.push([company.name + " \x02(" + company.performance + ")\x02", "£" + bot.plugins.util.thSeps(company.value)]);
 				});
+
+				bot.say(to, bot.plugins.bits.format(prefix, bits));
+			});
+		},
+
+		"cmd#openttdcomp": function(nick, to, args) {
+			var comp = args[1];
+			var server = args[2] || self.defServer;
+			var port = args[3] || self.defPort;
+			openttd.query(server, port, function(ret) {
+				var companies = ret[3].companies;
+
+				var i;
+				for (i = 0; i < companies.length && companies[i].id != comp && companies[i].name.toLowerCase().indexOf(comp.toLowerCase()) < 0; i++); // maybe unneccessary find
+				if (i == companies.length) // not found
+					return;
+
+				var company = companies[i];
+
+				var prefix = company.name + " on " + ret[1].name;
+				var bits = [];
+
+				bits.push(["start year", company.startYear]);
+				bits.push(["value", "£" + bot.plugins.util.thSeps(company.value)]);
+				bits.push(["money", "£" + bot.plugins.util.thSeps(company.money)]);
+				bits.push(["income", "£" + bot.plugins.util.thSeps(company.income)]);
+				bits.push(["performance", company.performance]);
+
+				/*bits.push(["vehicles", Object.keys(company.vehicles).map(function(type) {
+					return type + ": " + company.vehicles[type];
+				}).join(", ")]);
+				bits.push(["stations", Object.keys(company.stations).map(function(type) {
+					return type + ": " + company.stations[type];
+				}).join(", ")]);*/
+
+				bits.push(["type", "vehicles (stations)"]);
+				for (var type in company.vehicles) {
+					bits.push([type, company.vehicles[type] + " (" + company.stations[type] + ")"]);
+				}
 
 				bot.say(to, bot.plugins.bits.format(prefix, bits));
 			});

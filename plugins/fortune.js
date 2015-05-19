79cb73383de55f540bedab3fb233a4df9d338808
Simmo Saan <simmo.saan@gmail.com>
2015-05-19 19:21:04 +0300

    fortune: add custom quotes support

--- plugins/fortune.js
+++ plugins/fortune.js
@@ -6,6 +6,18 @@ function FortunePlugin(bot) {
 	self.help = "Fortune teller plugin";
 	self.depend = ["cmd"];
 
+	self.quoteDir = null;
+
+	self.load = function(data) {
+		if (data) {
+			self.quoteDir = data.quoteDir || null;
+		}
+	};
+
+	self.save = function() {
+		return {quoteDir: self.quoteDir};
+	};
+
 	self.events = {
 		"cmd#fortune" : function(nick, to, args, message) {
 			var arr = ["-s"];
@@ -45,6 +57,27 @@ function FortunePlugin(bot) {
 			});
 		},
 
+		"cmd#quote" : function(nick, to, args, message) {
+			if (self.quoteDir !== null) {
+				var arr = ["-s"/*, "-c"*/, self.quoteDir]; // TODO: nice output of source fortunes file
+				if (args[1] && args[1].match(/^\w+$/))
+					arr[arr.length - 1] += args[1];
+				execFile("fortune", arr, {timeout: 1000}, function (error, stdout, stderr) {
+					if (error && error.killed === true)
+						bot.say(to, nick + ": no such fortune found");
+
+					stdout.split("\n").forEach(function (line) {
+						if (line != "")
+							bot.say(to, "  " + line);
+					});
+					stderr.split("\n").forEach(function (line) {
+						if (line != "")
+							bot.say(to, "  " + line);
+					});
+				});
+			}
+		},
+
 		"cmd#fortunes": function (nick, to, args) {
 			execFile("fortune", ["-f"], function (error, stdout, stderr) {
 				var arr = stderr.split("\n").map(function (line) {
@@ -63,6 +96,18 @@ function FortunePlugin(bot) {
 
 				bot.notice(nick, "All available =fortuneo categories: " + arr.join(", "));
 			});
+		},
+
+		"cmd#quotes": function (nick, to, args) {
+			if (self.quoteDir !== null) {
+				execFile("fortune", ["-f", self.quoteDir], function (error, stdout, stderr) {
+					var arr = stderr.split("\n").map(function (line) {
+						return line.trim().split(" ")[1];
+					}).slice(1, -1);
+
+					bot.notice(nick, "All available =quote categories: " + arr.join(", "));
+				});
+			}
 		}
 	};
 }

49592efc2793d11677177935662c8f8f4f13e113
Simmo Saan <simmo.saan@gmail.com>
2015-05-22 09:01:30 +0300

    fortune: default args for fortune command calls

--- plugins/fortune.js
+++ plugins/fortune.js
@@ -35,7 +35,7 @@ function FortunePlugin(bot) {
 	};
 
 	self.fortune = function(arr, callback) {
-		execFile("fortune", arr, {timeout: 1000}, function (error, stdout, stderr) {
+		execFile("fortune", ["-s"].concat(arr), {timeout: 1000}, function (error, stdout, stderr) {
 			if (error && error.killed === true)
 				callback(nick + ": no such fortune found");
 
@@ -51,7 +51,7 @@ function FortunePlugin(bot) {
 	};
 
 	self.fortunes = function(arr, callback) {
-		execFile("fortune", arr, function (error, stdout, stderr) {
+		execFile("fortune", ["-f"].concat(arr), function (error, stdout, stderr) {
 			var arr = stderr.split("\n").map(function (line) {
 				return line.trim().split(" ")[1];
 			}).slice(1, -1);
@@ -62,7 +62,7 @@ function FortunePlugin(bot) {
 
 	self.events = {
 		"cmd#fortune" : function(nick, to, args, message) {
-			var arr = ["-s"];
+			var arr = [];
 			if (args[1] && args[1].match(/^\w+$/))
 				arr.push(args[1]);
 
@@ -72,9 +72,9 @@ function FortunePlugin(bot) {
 		},
 
 		"cmd#fortuneo" : function(nick, to, args, message) {
-			var arr = ["-s", "/usr/share/games/fortunes/off/"];
+			var arr = ["/usr/share/games/fortunes/off/"];
 			if (args[1] && args[1].match(/^\w+$/))
-				arr.push(args[1]);
+				arr[arr.length - 1] += args[1];
 
 			self.fortune(arr, function(line) {
 				bot.say(to, line);
@@ -83,7 +83,7 @@ function FortunePlugin(bot) {
 
 		"cmd#quote" : function(nick, to, args, message) {
 			if (self.quoteDir !== null) {
-				var arr = ["-s"/*, "-c"*/, self.quoteDir]; // TODO: nice output of source fortunes file
+				var arr = [/*"-c",*/ self.quoteDir]; // TODO: nice output of source fortunes file
 				if (args[1] && args[1].match(/^\w+$/))
 					arr[arr.length - 1] += args[1].toLowerCase();
 
@@ -94,20 +94,20 @@ function FortunePlugin(bot) {
 		},
 
 		"cmd#fortunes": function (nick, to, args) {
-			self.fortunes(["-f"], function(arr) {
+			self.fortunes([], function(arr) {
 				bot.notice(nick, "All available =fortune categories: " + arr.join(", "));
 			});
 		},
 
 		"cmd#fortuneso": function (nick, to, args) {
-			self.fortunes(["-f", "-o"], function(arr) {
+			self.fortunes(["-o"], function(arr) {
 				bot.notice(nick, "All available =fortuneo categories: " + arr.join(", "));
 			});
 		},
 
 		"cmd#quotes": function (nick, to, args) {
 			if (self.quoteDir !== null) {
-				self.fortunes(["-f", self.quoteDir], function(arr) {
+				self.fortunes([self.quoteDir], function(arr) {
 					bot.notice(nick, "All available =quote categories: " + arr.join(", "));
 				});
 			}

a025ed13d5a992946a7c45261b62dfefc3578ed5
Simmo Saan <simmo.saan@gmail.com>
2015-07-20 15:07:42 +0300

    log: =history like syntax (closes #47)

--- plugins/log.js
+++ plugins/log.js
@@ -6,18 +6,64 @@ function LogPlugin(bot) {
 	self.help = "simbot log viewer plugin";
 	self.depend = ["cmd", "auth"];
 
+	self.grepRe = new RegExp(
+		"(/)((?:\\\\\\1|(?!\\1).)+)" +
+		"\\1([a-z])*"); // simplified from sed plugin // copied from history plugin
+
+	self.iterate = function(lineCb, endCb) {
+		var found = false;
+		fs.readFile("./data/simbot.log", {encoding: "utf8"}, function(err, data) {
+			if (err)
+				throw err;
+
+			var lines = data.split("\n");
+
+			for (var j = lines.length - 1 - 1; !found && j >= 0; j--) {
+				if (!lineCb(lines[j]))
+					found = true;
+			}
+
+			(endCb || function(){})(found);
+		});
+	};
+
 	self.events = {
 		"cmd#log": bot.plugins.auth.proxyEvent(10, function(nick, to, args) {
-			var linecnt = args[1] || 20;
+			// simplified from history plugin
+			var linecnt;
+			var re = null;
 
-			fs.readFile("./data/simbot.log", {encoding: "utf8"}, function(err, data) {
-				if (err)
-					throw err;
+			for (var i = 1; i < args.length; i++) {
+				var arg = args[i];
 
-				var lines = data.split("\n").slice(-linecnt);
+				if (arg.match(/^\d+/))
+					linecnt = parseInt(arg);
+				else {
+					var m = arg.match(self.grepRe);
+					if (m) {
+						re = new RegExp(m[2], m[3]);
+					}
+				}
+			}
 
-				for (var i = 0; i < lines.length; i++)
-					bot.say(to, lines[i]); // TODO: add IRC color to lines
+			linecnt = Math.min(linecnt || 15, 50);
+
+			var outlines = [];
+			self.iterate(function(line) {
+				if (re === null || line.match(re)) {
+					outlines.unshift(re !== null ? line.replace(re, "\x16$&\x16") : line); // highlight matches by color reversal
+
+					linecnt--;
+				}
+
+				return linecnt > 0;
+			}, function() {
+				bot.say(nick, "\x031--- Begin log ---");
+				for (var i = 0; i < outlines.length; i++) {
+					var str = outlines[i];
+					bot.say(nick, str);
+				}
+				bot.say(nick, "\x031--- End log ---");
 			});
 		})
 	};

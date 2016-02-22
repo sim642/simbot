df743c5d5c1a9ac0b02682aaebd3ec6897537e8b
Simmo Saan <simmo.saan@gmail.com>
2016-02-22 19:44:34 +0200

    info: fix error when called without arguments, change multiple info syntax

--- plugins/info.js
+++ plugins/info.js
@@ -18,30 +18,32 @@ function InfoPlugin(bot) {
 	};
 
 	self.multiinfo = function(nicks, callback) {
-		pg.connect(self.conStr, function(err, client, done) {
-			if (err) {
-				bot.out.error("info", err);
-			}
-
-			client.query("SELECT alias, nick, info FROM multiinfo($1)", [nicks], function(err, result) {
-				done();
+		if (nicks) {
+			pg.connect(self.conStr, function(err, client, done) {
 				if (err) {
 					bot.out.error("info", err);
 				}
 
-				var ret = {};
+				client.query("SELECT alias, nick, info FROM multiinfo($1)", [nicks], function(err, result) {
+					done();
+					if (err) {
+						bot.out.error("info", err);
+					}
 
-				for (var i = 0; i < result.rowCount; i++) {
-					var row = result.rows[i];
-					ret[row.alias] = {
-						alias: row.nick !== null && row.alias.toLowerCase() != row.nick.toLowerCase() ? row.nick : null,
-						info: row.info
-					};
-				}
+					var ret = {};
 
-				callback(ret);
+					for (var i = 0; i < result.rowCount; i++) {
+						var row = result.rows[i];
+						ret[row.alias] = {
+							alias: row.nick !== null && row.alias.toLowerCase() != row.nick.toLowerCase() ? row.nick : null,
+							info: row.info
+						};
+					}
+
+					callback(ret);
+				});
 			});
-		});
+		}
 	};
 
 	self.info = function(nicks, callback) {
@@ -55,7 +57,7 @@ function InfoPlugin(bot) {
 
 	self.events = {
 		"cmd#infobot": function(nick, to, args, message) {
-			self.info(args[1].split(","), function(nick, alias, info) {
+			self.info(args.slice(1), function(nick, alias, info) {
 				bot.say(to, "Info of " + nick + (alias !== null ? " (" + alias + ")" : "") + ": " + info);
 			});
 		}

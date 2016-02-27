eed327678cffa4faa118b53cfe73e28a1498852c
Simmo Saan <simmo.saan@gmail.com>
2016-02-27 21:05:41 +0200

    wiki: add alternate query function for MediaWikis without TextExtracts

--- plugins/wiki.js
+++ plugins/wiki.js
@@ -1,4 +1,6 @@
 var request = require("request");
+var dom = require("xmldom").DOMParser;
+var xpath = require("xpath");
 var util = require("util");
 
 function WikiPlugin(bot) {
@@ -6,6 +8,12 @@ function WikiPlugin(bot) {
 	self.name = "wiki";
 	self.help = "MediaWiki plugin";
 	self.depend = ["cmd", "bitly"];
+
+	self.ellipsize = function(text, length) {
+		var ellipsis = "...";
+		var limit = length - ellipsis.length;
+		return text.length > limit ? text.substr(0, limit) + ellipsis : text;
+	};
 
 	self.query = function(root, text, callback) {
 		/* https://www.mediawiki.org/wiki/Extension:TextExtracts */
@@ -58,10 +66,85 @@ function WikiPlugin(bot) {
 		});
 	};
 
+	self.query2 = function(root, text, callback) {
+		/* https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&format=json&prop=info&generator=search&redirects=1&inprop=url&gsrsearch=Linus+Torvald&gsrlimit=1&gsrenablerewrites=1 */
+		/* /w/api.php?action=query&format=json&prop=info&generator=search&redirects=1&inprop=url&gsrsearch=Linus+Torvald&gsrlimit=1&gsrenablerewrites=1 */
+
+		/* https://en.wikipedia.org/wiki/Special:ApiSandbox#action=parse&format=json&pageid=17618&prop=text&section=0 */
+		/* /w/api.php?action=parse&format=json&pageid=17618&prop=text&section=0 */
+
+		request({
+			url: root,
+			qs: {
+				"action": "query",
+				"redirects": "",
+
+				"generator": "search",
+				"gsrsearch": text,
+				"gsrlimit": 1,
+				"gsrenablerewrites": "",
+
+				"prop": "info",
+
+				"inprop": "url",
+
+				"format": "json"
+			}
+		}, function(err, res, body) {
+			var data = JSON.parse(body);
+			//bot.out.debug("wiki", util.inspect(data, {colors: true, depth: null}));
+
+			if (data.query && data.query.pages) {
+				for (var pageid in data.query.pages) {
+					var page = data.query.pages[pageid];
+
+					request({
+						url: root,
+						qs: {
+							"action": "parse",
+
+							"pageid": page.pageid,
+
+							"prop": "text",
+							"section": 0,
+
+							"format": "json"
+						}
+					}, function(err, res, body) {
+						var data = JSON.parse(body);
+						var html = data.parse.text["*"];
+
+						var doc = new dom().parseFromString(html);
+						var text = xpath.select("./p//text()", doc).map(function(node) {
+							return node.nodeValue;
+						}).join("").replace(/[\r\n]/g, " ");
+						text = self.ellipsize(text, 350);
+
+						bot.plugins.bitly.shorten(page.fullurl, function(shorturl) {
+							var str = "\x02" + page.title + "\x02: " + text + " \x1F" + shorturl;
+							callback(str);
+						});
+					});
+				}
+			}
+			else {
+				callback("\x02" + text + "\x02: not found");
+			}
+		});
+	};
+
 	self.events = {
 		"cmd#wikipedia": function(nick, to, args) {
 			if (args[0].trim().length > 0) {
 				self.query("https://en.wikipedia.org/w/api.php", args[0], function(str) {
+					bot.say(to, str);
+				});
+			}
+		},
+
+		"cmd#wikipedia2": function(nick, to, args) {
+			if (args[0].trim().length > 0) {
+				self.query2("https://en.wikipedia.org/w/api.php", args[0], function(str) {
 					bot.say(to, str);
 				});
 			}

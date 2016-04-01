caa40fd136641cb15ec32ef395f8647a027b284e
Simmo Saan <simmo.saan@gmail.com>
2016-04-01 13:51:22 +0300

    politeness: new plugin for FoxType politeness calculation

---
+++ plugins/politeness.js
@@ -0,0 +1,41 @@
+var request = require("request");
+
+function PolitenessPlugin(bot) {
+	var self = this;
+	self.name = "politeness";
+	self.help = "FoxType Labs politeness plugin";
+	self.depend = ["cmd"];
+
+	self.politeness = function(text, callback) {
+		request.post({
+			url: "https://api-classifier.foxtype.com/v1/all/politeness02",
+			body: {
+				"text": text,
+			},
+			json: true
+		}, function(err, res, body) {
+			if (!err && res.statusCode == 200) {
+				callback(body);
+			}
+			else {
+				bot.out.error("politeness", body);
+			}
+		});
+	};
+
+	self.overall = function(text, callback) {
+		self.politeness(text, function(body) {
+			callback(body.overall.politeness.scoreBinProb);
+		});
+	};
+
+	self.events = {
+		"cmd#politeness": function(nick, to, args) {
+			self.overall(args[0], function(politeness) {
+				bot.say(to, nick + ": " + (politeness * 100) + "%");
+			});
+		}
+	};
+}
+
+module.exports = PolitenessPlugin;

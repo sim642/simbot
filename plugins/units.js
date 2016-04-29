be27f2d550c477d22cbb528316e2c7a57c7dbf9d
Simmo Saan <simmo.saan@gmail.com>
2016-04-29 20:35:11 +0000

    units: prevent search command from producing spammy output

--- plugins/units.js
+++ plugins/units.js
@@ -6,15 +6,22 @@ function UnitsPlugin(bot) {
 	self.help = "Unit conversion plugin";
 	self.depend = ["cmd"];
 
+	self.unitListRe = /\?|search/i; // disallow spammy input
+
 	self.events = {
 		"cmd#units" : function(nick, to, args, message) {
-			if (args[2] === undefined || (args[2] !== undefined && args[2].trim() != "?")) {
-				var inp = (args[1] || "") + "\n" + (args[2] || "") + "\n";
+			if (args.length == 0)
+				return;
+			if (args[1] !== undefined && self.unitListRe.test(args[1]))
+				return;
+			if (args[2] !== undefined && self.unitListRe.test(args[2]))
+				return;
 
-				execFile("units", ["-t"], function (error, stdout, stderr) {
-					bot.say(to, nick + ": " + stdout.replace(/\t/g, "").replace(/\n/g, "; "));
-				}).stdin.end(inp);
-			}
+			var inp = (args[1] || "") + "\n" + (args[2] || "") + "\n";
+
+			execFile("units", ["-t"], function (error, stdout, stderr) {
+				bot.say(to, nick + ": " + stdout.replace(/\t/g, "").replace(/\n/g, "; "));
+			}).stdin.end(inp);
 		}
 	};
 }

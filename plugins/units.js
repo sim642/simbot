265fd9120a88f06889a4936577883400a5fda76e
Simmo Saan <simmo.saan@gmail.com>
2016-04-29 20:26:28 +0000

    units: use system default units installation

--- plugins/units.js
+++ plugins/units.js
@@ -11,7 +11,7 @@ function UnitsPlugin(bot) {
 			if (args[2] === undefined || (args[2] !== undefined && args[2].trim() != "?")) {
 				var inp = (args[1] || "") + "\n" + (args[2] || "") + "\n";
 
-				execFile("./plugins/units/units", ["-f", "./plugins/units/definitions.units", "-t"], function (error, stdout, stderr) {
+				execFile("units", ["-t"], function (error, stdout, stderr) {
 					bot.say(to, nick + ": " + stdout.replace(/\t/g, "").replace(/\n/g, "; "));
 				}).stdin.end(inp);
 			}

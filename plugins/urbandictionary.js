7d0da8a3071b715c086152ac5389e66763a32225
Simmo Saan <simmo.saan@gmail.com>
2018-07-30 09:04:14 +0000

    urbandictionary: new plugin

---
+++ plugins/urbandictionary.js
@@ -0,0 +1,63 @@
+var request = require("request");
+
+function UrbanDictionaryPlugin(bot) {
+	var self = this;
+	self.name = "urbandictionary";
+	self.help = "Urban Dictionary plugin";
+	self.depend = ["cmd"];
+
+	self.events = {
+		"cmd#ud": function(nick, to, args) {
+			if (args[1] == "random") {
+				request("https://api.urbandictionary.com/v0/random", function(err, res, body) {
+					if (!err && res.statusCode == 200) {
+						var j = JSON.parse(body);
+
+						if (j.list !== undefined && j.list.length !== 0) {
+							var item = j.list[0];
+							bot.say(to, "\x02" + item.word + "\x02 [random]: " + item.definition);
+						}
+					}
+				});
+			}
+			else {
+				var match = args[0].match(/^(.*)\s+(\d+)\s*$/);
+				var str;
+				var i;
+				if (match) {
+					str = match[1];
+					i = parseInt(match[2]);
+				}
+				else {
+					str = args[0];
+					i = 1;
+				}
+				str = str.trim();
+
+				request({
+						uri: "https://api.urbandictionary.com/v0/define",
+						qs: {
+							term: str
+						}
+					}, function(err, res, body) {
+					if (!err && res.statusCode == 200) {
+						var j = JSON.parse(body);
+
+						if (j.list !== undefined && j.list.length !== 0) {
+							if (i - 1 >= 0 && i - 1 < j.list.length) {
+								var item = j.list[i - 1];
+								bot.say(to, "\x02" + item.word + "\x02 [" + i + "/" + j.list.length + "]: " + item.definition);
+							}
+							else
+								bot.say(to, nick + ": \x02" + str + "\x02 [" + i + "/" + j.list.length + "] invalid result index");
+						}
+						else
+							bot.say(to, nick + ": couldn't find \x02" + str + "\x02");
+					}
+				});
+			}
+		}
+	};
+}
+
+module.exports = UrbanDictionaryPlugin;
